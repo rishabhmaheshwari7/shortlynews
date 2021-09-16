aa3e3a8a47f261c6ad45d95bccfaaaa97c84183b
rishabhmaheshwari7 <47009218+rishabhmaheshwari7@users.noreply.github.com>
2021-09-17 01:55:11 +0530

    Update urls.py

--- DataFlair_NewsAggregator/urls.py
+++ DataFlair_NewsAggregator/urls.py
@@ -21,7 +21,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 from django.views.static import serve
-from django.conf.urls import urls
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
