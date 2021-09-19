37d2d4abf094cb7860f0835a12f9a216ad250f87
rishabhmaheshwari7 <47009218+rishabhmaheshwari7@users.noreply.github.com>
2021-09-19 12:46:57 +0530

    Update urls.py

--- news/urls.py
+++ news/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from news.views import scrape1,scrape2, news_list
 
 urlpatterns = [
+	path('', news_list, name="home"),
 	path('scrape1/', scrape1, name="scrape1"),
 	path('scrape2/', scrape2, name="scrape2"),
-	path('', news_list, name="home"),
+	
 ]
